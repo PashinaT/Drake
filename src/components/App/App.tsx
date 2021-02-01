a26eef01d2332ecefb8f81b952e063e583377757
pashinaT <pashinatania@mail.ru>
2021-02-02 00:04:10 +0400

    drake calc

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import '../../styles/styles.css';
+import DrakeCalc from "../DrakeCalc/DrakeCalc";
+
+function App() {
+  return (
+      <DrakeCalc/>
+  );
+}
+
+export default App;
